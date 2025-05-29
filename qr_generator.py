f9ed329512812b35f17d203448698a14b200cf68
Shafee25 <shafeeahamed494@gmail.com>
2025-05-29 21:17:28 +0530

    finalize in qr

--- qr_generator.py
+++ qr_generator.py
@@ -1,5 +1,11 @@
 import qrcode
+from pyzbar.pyzbar import decode
+from PIL import Image
 
 myqr = qrcode.make("https://www.youtube.com/watch?v=IUQVO97zcE0&t=2s")
 
 myqr.save("myqr.png", scale = 8)
+
+img = decode(Image.open("myqr.png"))
+
+print(img[0].data.decode("ascii"))
